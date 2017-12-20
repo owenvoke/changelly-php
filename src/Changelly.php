73641f21f8a74411189171e2a4163f0e1c069540
pxgamer <owzie123@gmail.com>
2017-12-20 15:05:11 +0000

    Initial class

---
+++ src/Changelly.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pxgamer\Changelly;
+
+/**
+ * Class Changelly
+ */
+class Changelly
+{
+    /**
+     * The base URI for the Changelly main site.
+     */
+    public const BASE_URI = 'http://changelly.com';
+    /**
+     * The base URI for the Changelly API.
+     */
+    public const API_BASE_URI = 'http://api.changelly.com';
+}
