78d795f327944fe7f80ec1df497322d9418e98ec
pxgamer <owzie123@gmail.com>
2017-12-20 16:33:19 +0000

    Updated the ApiCallable trait

--- src/Traits/ApiCallable.php
+++ src/Traits/ApiCallable.php
@@ -4,11 +4,29 @@ namespace pxgamer\Changelly\Traits;
 
 use GuzzleHttp\Client;
 use pxgamer\Changelly\Changelly;
+use pxgamer\Changelly\Message;
 
+/**
+ * Trait ApiCallable
+ */
 trait ApiCallable
 {
+    /**
+     * @var Client
+     */
     protected $client;
+    /**
+     * @var string
+     */
+    protected $secret;
+    /**
+     * @var string
+     */
+    protected $apiKey;
 
+    /**
+     * ApiCallable constructor.
+     */
     public function __construct()
     {
         $this->client = new Client([
@@ -16,8 +34,44 @@ trait ApiCallable
         ]);
     }
 
-    public function call(array $message)
+    /**
+     * @param Message $message
+     *
+     * @return mixed
+     */
+    public function call(Message $message)
     {
+        return \GuzzleHttp\json_decode(
+            $this->client
+                ->post(
+                    Changelly::API_BASE_URI,
+                    [
+                        'headers' => [
+                            'Content-Type' => 'application/json',
+                            'api-key'      => $this->apiKey,
+                            'sign'         => $message->getSigned($this->secret),
+                        ],
+                        'json'    => $message->get(),
+                    ]
+                )
+                ->getBody()
+                ->getContents()
+        );
+    }
 
+    /**
+     * @param string $secret
+     */
+    public function setSecret(string $secret): void
+    {
+        $this->secret = $secret;
+    }
+
+    /**
+     * @param string $apiKey
+     */
+    public function setApiKey(string $apiKey): void
+    {
+        $this->apiKey = $apiKey;
     }
 }
